eef3d6e1e0f2c53909b63a98b261893062dacfb7
894620576@qq.com <cheney->
2019-04-27 19:09:20 +0800

    update layout

---
+++ src/views/layout/head.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-toolbar dark fixed color="primary">
+    <v-btn icon style="margin-right:18px;">
+      <v-icon>apps</v-icon>
+    </v-btn>
+
+    <router-link
+      :to="item.path"
+      v-ripple
+      class="nav-link"
+      v-for="(item,index) in menus"
+      :key="index"
+    >{{item.title}}</router-link>
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>github-circle</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      menus: []
+    };
+  },
+  created() {
+    const { options } = this.$router;
+    this.menus = options.routes[0].children.map(item => {
+      item["active"] = false;
+      return item;
+    });
+  },
+  methods: {}
+};
+</script>
+<style lang="scss" scoped>
+.nav-link {
+  font-size: 16px;
+  font-weight: 700;
+  display: block;
+  color: #ffffff;
+  line-height: 64px;
+  padding: 0 15px;
+  text-decoration: none;
+}
+.nav-link.router-link-active {
+  background: rgba($color: #ffffff, $alpha: 0.2);
+}
+</style>
--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -1,38 +1,32 @@
 <template>
-    <div>
-        <div id="nav">
-            <router-link to="/">Home</router-link>
-            |
-            <router-link to="/about">About</router-link>
-        </div>
-        <router-view />
-    </div>
+  <div>
+    <Head/>
+    <div class="zwf"></div>
+    <router-view/>
+  </div>
 </template>
 <script>
-    export default {
-        data() {
-            return {
-                collapsed: false,
-            }
-        },
-    }
+import Head from "./head.vue";
+export default {
+  components: {
+    Head
+  },
+  data() {
+    return {
+      collapsed: false,
+      admins: [["Management", "people_outline"], ["Settings", "settings"]],
+      cruds: [
+        ["Create", "add"],
+        ["Read", "insert_drive_file"],
+        ["Update", "update"],
+        ["Delete", "delete"]
+      ]
+    };
+  }
+};
 </script>
 <style>
-    #components-layout-demo-custom-trigger .trigger {
-        font-size: 18px;
-        line-height: 64px;
-        padding: 0 24px;
-        cursor: pointer;
-        transition: color .3s;
-    }
-
-    #components-layout-demo-custom-trigger .trigger:hover {
-        color: #1890ff;
-    }
-
-    #components-layout-demo-custom-trigger .logo {
-        height: 32px;
-        background: rgba(255, 255, 255, .2);
-        margin: 16px;
-    }
+.zwf {
+  height: 64px;
+}
 </style>
