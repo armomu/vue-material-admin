b73c2b666fc7384243861f260974e661f31b16be
894620576@qq.com <cheney->
2019-05-28 11:43:23 +0800

    build for i18n

---
+++ js/moving.d8e4eb1f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["moving"],{1887:function(t,e,n){"use strict";n.r(e);var c=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("动态")])])}],a=n("17cc"),l={},r=Object(a["a"])(l,c,s,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=moving.d8e4eb1f.js.map
