2dbcd1806ca3dd22e083bd230f356e26f9819e57
894620576@qq.com <cheney->
2019-09-15 13:23:20 +0800

    :wrench: :pencil2:Rename

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/Vuetify-todo/dist' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/vue-material-admin/dist' : '/',
 };
