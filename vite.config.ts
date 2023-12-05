3da106bab0562c345238b329bafdc1575919ee73
陈华杰 <894620576@qq.com>
2023-12-05 18:09:54 +0800

    /ᐠ｡ꞈ｡ᐟ\ ᑋᵉᑊᑊᵒ ᵕ̈

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { VitePWA } from 'vite-plugin-pwa';
-import vueJsx from '@vitejs/plugin-vue-jsx';
 import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
@@ -11,41 +9,41 @@ export default defineConfig({
     base: '/vue-material-admin',
     plugins: [
         vue(),
-        vueJsx(),
+        // vueJsx(),
         vuetify() as any,
-        VitePWA({
-            registerType: 'autoUpdate',
-            devOptions: {
-                enabled: true,
-            },
-            manifest: {
-                name: 'Material UI',
-                short_name: 'Material UI',
-                icons: [
-                    {
-                        purpose: 'maskable',
-                        src: 'http://storage.ckudz.com/20230209150833_icon-192.png?e=1991286513&token=6p-iHOSmyL95FPwRy4pd3OXJFLFxCp0JnSqDqF0_:jeIPaBn-qgiVU4He5cnSURC7XUg=',
-                        type: 'image/png',
-                        sizes: '192x192',
-                    },
-                    {
-                        src: 'http://storage.ckudz.com/20230209150833_icon-192.png?e=1991286513&token=6p-iHOSmyL95FPwRy4pd3OXJFLFxCp0JnSqDqF0_:jeIPaBn-qgiVU4He5cnSURC7XUg=',
-                        type: 'image/png',
-                        sizes: '64x64 32x32 24x24 16x16',
-                    },
-                    {
-                        src: 'https://storage.ckudz.com/20230209150923_icon-512.png?e=1991286563&token=6p-iHOSmyL95FPwRy4pd3OXJFLFxCp0JnSqDqF0_:4WQvV-efArRA8xWaAxQK_kt5ei8=',
-                        type: 'image/png',
-                        sizes: '128x128 144x144 152x152 192x192 256x256 512x512',
-                    },
-                ],
-                scope: '/vue-material-admin/',
-                start_url: '/vue-material-admin/',
-                background_color: '#FFFFFF',
-                theme_color: '#FFFFFF',
-                display: 'standalone',
-            },
-        }),
+        // VitePWA({
+        //     registerType: 'autoUpdate',
+        //     devOptions: {
+        //         enabled: true,
+        //     },
+        //     manifest: {
+        //         name: 'Material UI',
+        //         short_name: 'Material UI',
+        //         icons: [
+        //             {
+        //                 purpose: 'maskable',
+        //                 src: 'http://storage.ckudz.com/20230209150833_icon-192.png?e=1991286513&token=6p-iHOSmyL95FPwRy4pd3OXJFLFxCp0JnSqDqF0_:jeIPaBn-qgiVU4He5cnSURC7XUg=',
+        //                 type: 'image/png',
+        //                 sizes: '192x192',
+        //             },
+        //             {
+        //                 src: 'http://storage.ckudz.com/20230209150833_icon-192.png?e=1991286513&token=6p-iHOSmyL95FPwRy4pd3OXJFLFxCp0JnSqDqF0_:jeIPaBn-qgiVU4He5cnSURC7XUg=',
+        //                 type: 'image/png',
+        //                 sizes: '64x64 32x32 24x24 16x16',
+        //             },
+        //             {
+        //                 src: 'https://storage.ckudz.com/20230209150923_icon-512.png?e=1991286563&token=6p-iHOSmyL95FPwRy4pd3OXJFLFxCp0JnSqDqF0_:4WQvV-efArRA8xWaAxQK_kt5ei8=',
+        //                 type: 'image/png',
+        //                 sizes: '128x128 144x144 152x152 192x192 256x256 512x512',
+        //             },
+        //         ],
+        //         scope: '/vue-material-admin/',
+        //         start_url: '/vue-material-admin/',
+        //         background_color: '#FFFFFF',
+        //         theme_color: '#FFFFFF',
+        //         display: 'standalone',
+        //     },
+        // }),
     ],
     server: {
         open: true,
