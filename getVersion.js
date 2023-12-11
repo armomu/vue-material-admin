12eacd1f5dbcd4dfa5c02e2a981fb33e8b99b651
陈华杰 <894620576@qq.com>
2023-12-11 11:18:38 +0800

    Chore version script

--- getVersion.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-undef
-const fs = require('fs');
-console.log('build version');
-fs.writeFile('./public/version.js', `getVersion('${new Date().getTime()}')`, function (err) {
-    if (err) {
-        console.log(err);
-        console.log('文件创建或写入失败');
-    }
-});
