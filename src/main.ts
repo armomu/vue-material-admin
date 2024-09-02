a80f97681e2bb5732cb0fd4df6852d7ee5d36f93
陈华杰 <894620576@qq.com>
2024-09-02 15:20:15 +0800

    delete unreferenced packages

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,9 @@ import { vuetify } from '@/plugins/vuetify';
 import '@/plugins/pwa';
 import registeComponent from './components';
 
-// only use oreo-editor
-import ArcoVue from '@arco-design/web-vue';
-import ArcoVueIcon from '@arco-design/web-vue/es/icon';
-import '@arco-design/web-vue/dist/arco.css';
 const app = createApp(App);
 app.use(createPinia());
 app.use(vuetify);
-app.use(ArcoVue); // only use oreo-editor
-app.use(ArcoVueIcon); // only use oreo-editor
 app.use(router);
 
 registeComponent(app);
