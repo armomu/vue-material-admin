3f925e925eee04ded6aa1e594fa46351ddba3f42
陈华杰 <894620576@qq.com>
2024-10-18 16:38:00 +0800

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -179,7 +179,7 @@ module.exports = {
         'no-this-before-super': 2,
         'no-throw-literal': 2,
         'no-trailing-spaces': 2,
-        'no-undef': 1,
+        'no-undef': 2,
         'no-undef-init': 2,
         'no-unexpected-multiline': 2,
         'no-unmodified-loop-condition': 2,
