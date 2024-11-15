83f77a359b7b7896e44c28e905fdebe44effe352
陈华杰 <894620576@qq.com>
2024-11-15 18:37:23 +0800

    Create babylonjs.vue

---
+++ src/views/graphics/babylonjs.vue
@@ -0,0 +1,18 @@
+<template>
+    <div style="position: relative">
+        <v-card
+            append-icon="mdi-open-in-new"
+            href="https://github.com/armomu/ergoudan"
+            rel="noopener"
+            target="_blank"
+            title="GitHub"
+            style="position: absolute; right: 32px; top: 16px; z-index: 9"
+        ></v-card>
+        <iframe
+            src="https://armomu.github.io/ergoudan/#/"
+            style="width: 100%; height: var(--content-height); border: none"
+        >
+        </iframe>
+    </div>
+</template>
+<script setup lang="ts"></script>
