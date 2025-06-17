b4c1df22a641931fece6610620fb3ed72974dcb5
陈华杰 <894620576@qq.com>
2025-06-17 15:08:01 +0800

    chore tips

--- src/views/oreo-editor/index.vue
+++ src/views/oreo-editor/index.vue
@@ -3,19 +3,17 @@
         src="https://armomu.github.io/oreo-editor/#/"
         style="width: 100%; height: var(--content-height); border: none"
     />
-    <v-dialog v-model="dialog" width="30%">
-        <v-card title="Dialog">
-            <v-card-text>
-                oreo-editor H5编辑器源代码已经独立到 https://github.com/armomu/oreo-editor 项目中
-            </v-card-text>
-            <v-card-actions>
-                <v-btn color="primary" block @click="dialog = false">收到 over！</v-btn>
-            </v-card-actions>
-        </v-card>
-    </v-dialog>
+    <v-snackbar v-model="snackbar">
+        The source code of oreo-editor H5 editor has been independently transferred to the
+        https://github.com/armomu/oreo-editor project
+
+        <template v-slot:actions>
+            <v-btn color="primary" variant="text" @click="snackbar = false"> Close </v-btn>
+        </template>
+    </v-snackbar>
 </template>
 <script lang="ts" setup>
 import { ref } from 'vue';
 
-const dialog = ref(true);
+const snackbar = ref(true);
 </script>
