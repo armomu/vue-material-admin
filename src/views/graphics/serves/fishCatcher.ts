93a6524f79b09f93a68fea4d0a6a41a1afd2c69c
陈华杰 <894620576@qq.com>
2025-08-20 18:05:57 +0800

    refactor: su7 page

--- src/views/graphics/serves/fishCatcher.ts
+++
@@ -1,4 +0,0 @@
-import * as PIXI from 'pixi.js';
-import { Button } from '@pixi/ui';
-
-export class FishCatcher {}
---
+++ src/views/graphics/su7Dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+    <div style="position: relative">
+        <iframe
+            src="https://lab-x-ui1p.vercel.app/#/su7"
+            style="width: 100%; height: var(--content-height); border: none"
+        >
+        </iframe>
+    </div>
+</template>
+<script setup lang="ts"></script>
