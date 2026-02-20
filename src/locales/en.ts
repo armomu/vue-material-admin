01924d00258bb4ef2c9c5de1232a5d3d93e8ada0
room2 <lukai@mail.ustc.edu.cn>
2026-02-20 23:14:36 +0800

    feat(locale): apply some translations to menu

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,8 +1,14 @@
 const en = {
     languageName: 'English',
     accessControl: 'Access Control',
+    Components: 'Components',
     Dashboard: 'Dashboard',
     Examples: 'Examples',
+    Graphics: 'Graphics',
+    'Role-Based Access Control': 'Role-Based Access Control',
+    'Smart House': 'Smart House',
+    'Tesla Model S': 'Tesla Model S',
+    'Tree Menu': 'Tree Menu',
 };
 
 export default en;
--- src/locales/zhHans.ts
+++ src/locales/zhHans.ts
@@ -1,8 +1,14 @@
 const zhHans = {
     languageName: '简体中文',
     accessControl: '权限控制',
+    Components: '组件',
     Dashboard: '主面板',
     Examples: '范例',
+    Graphics: '图形化',
+    'Role-Based Access Control': '角色权限控制',
+    'Smart House': '智能家居',
+    'Tesla Model S': '特斯拉 Model S',
+    'Tree Menu': '菜单树',
 };
 
 export default zhHans;
